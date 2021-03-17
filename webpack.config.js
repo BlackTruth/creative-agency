812d445702d087817d2cf6d478f0740b3f7960d4
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2021-03-17 10:55:03 +0300

    feat: configured project structure

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './dist',
+        open: 'chrome',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({template: './src/index.html'})],
+};
