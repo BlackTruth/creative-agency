9db2dfde8a65ed9edf4d32f2d7861b2f8aba9d2b
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2021-03-23 18:37:56 +0300

    feat: add html templates, add some assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devServer: {
-        contentBase: './dist',
-        open: 'chrome',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-        ],
-    },
-    plugins: [new HtmlWebpackPlugin({template: './src/index.html'})],
+  mode: "development",
+  entry: "./src/index.js",
+  devServer: {
+    contentBase: "./dist",
+    open: "chrome",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ca]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      header: { title: "Some Title" },
+      template: "./src/index.html",
+    }),
+  ],
 };
